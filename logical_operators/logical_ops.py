348ace50a0eec6bec641b52b6cfad94a33593f28
Tinoet <88379020+Tinoet@users.noreply.github.com>
2022-03-21 23:24:54 +0100

    Add files via upload

---
+++ logical_operators/logical_ops.py
@@ -0,0 +1,12 @@
+# logical operators AND  are used to combine two statements together
+has_high_income = False
+has_good_credit = True
+if has_high_income and has_good_credit:
+  print('eligible for loan')
+else:
+  print('not eligible for loan')
+
+  # logical operators or
+  # for AND: BOTH condition sshould true
+  # for OR : at least one should be true used to combine two statements together
+  # NOT
